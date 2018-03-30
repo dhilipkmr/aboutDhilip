7698e8da83daaf403147d6a7fa70c667ff00e5e3
dkvelusa <dkvelusa>
2018-03-30 19:16:24 +0530

    Responsive Header card

--- src/js/app.js
+++ src/js/app.js
@@ -6,11 +6,11 @@ var resume = angular.module('app',['ngRoute','controllers']);
 resume.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/',{
-            templateUrl : 'src/template/details.html',
+            templateUrl : 'src/template/material.html',
             controller : 'LandingController'
         })
-        .when('/material',{
-            templateUrl : 'src/template/material.html',
+        .when('/details',{
+            templateUrl : 'src/template/details.html',
             controller : 'LandingController'
         });
 }]);
