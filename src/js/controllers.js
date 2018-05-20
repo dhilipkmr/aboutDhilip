3a61cc41eda6eaf2bcacd873e11fdd7180c97728
Dhilip Kumar Velusamy <dhilip.velusamy@Go-Mac-DhilipV.local>
2018-05-21 00:51:36 +0530

    Minor Desription changes and skill update

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -14,7 +14,7 @@
 
         //About Me
         $scope.title1           = 'ABOUT ME';
-        $scope.shortDescription = 'A Software Developer with two Years of Experience, Specialising in Front-end Technologies with a keen Interest towards Programming';
+        $scope.shortDescription = 'A Software Developer with two years of Experience, specializing in Front-end Technologies with a keen interest towards Programming';
         $scope.contactDetails   = [
         ['Age',                 '23'],
         ['Address',             'B 103, Isha Casablanca'],
@@ -31,35 +31,37 @@
         $scope.technicalSkills1 = [
         ['Javascript', '85%'],
         ['JQuery', '90%'],
-        ['AngularJs', '60%'],
-        ['Bootstrap', '70%'],
-        ['React', '50%']
+        ['React', '75%']
+        ['Redux', '80%'],
+        ['AngularJs', '60%']
+        // ['Backbone', '80%'],
         ];
 
         //Left
         $scope.technicalSkills2 = [
-        ['Backbone', '80%'],
         ['HTML', '85%'],
         ['Css', '75%'],
+        ['Bootstrap', '70%'],
         ['Java', '65%'],
-        ['SQL', '80%']
+        ['SQL', '80%'],
+        ['Progressive Web Apps', '70%'],
         ];
 
         //Portfolio
         //Please add your font icon name in the second index of the array Preferred : Google fonts
         $scope.portfolioDetails =  [
-        ['DASHBOARD',           'fa-chart-line',  'A Summary Dashboard on Data Transfers Status for each Jobs to Identify & Rectify Errors'],
-        ['JOB SCHEDULE',        'fa-clock',  'Implemented Wizard which allows Users to Schedule a Job based on their Choice'],
-        ['QUICK SAVE',          'fa-save',  'Ability to Immediately Save Flows before the Last Screen, helped in faster modification of Flows'],
+        ['DASHBOARD',           'fa-chart-line',  'A Summary Dashboard on Data Transfers Status for each jobs to identify & rectify errors'],
+        ['JOB SCHEDULE',        'fa-clock',  'Implemented a Wizard which allows users to schedule a Job, based on their choice'],
+        ['QUICK SAVE',          'fa-save',  'Ability to immediately save Flows before the last screen which helped in faster modification of Flows'],
         ['TREE GRID',           'fa-th',  'Reduced the Page Loading time by implementing Lazy Load functionality as a part of Tree Grid'],
-        ['QUICK FILTERS',       'fa-filter',  'Condition based Record Filtering for Grids, based on Job\'s Start/End Time and Status'],
+        ['QUICK FILTERS',       'fa-filter',  'Condition-based Record Filtering for Grids, based on Job\'s Start/End Time and Status'],
         ['OBJECT MANIPULATION', 'fa-edit',  'Provided the ability to modify the configurations for Out of the Box Flows']
         ];
         
         //Education
         $scope.education        = [
         {
-        'name'                   : 'K.G. Matric. Hr. Sec. School',
+        'name'                  : 'K.G. Matric. Hr. Sec. School',
         'branch'                : 'Computer Science',
         'period'                : '2006-2012',
         'project'               : 'Library Management System'
@@ -77,7 +79,7 @@
         {
         'name'                  : 'Oracle',
         'role'                  : 'Intern',
-        'period'                : 'Jan 2016 - Jun'
+        'period'                : 'Jan 2016 - Jun 2016'
         },
         {
         'name'                  : 'Oracle',
@@ -87,7 +89,12 @@
         {
         'name'                  : 'Oracle',
         'role'                  : 'Applications Developer IC2',
-        'period'                : 'Dec 2017 - Present',
+        'period'                : 'Dec 2017 - Apr 2018',
+        },
+        {
+        'name'                  : 'Goibibo',
+        'role'                  : 'Senior Software Engineer',
+        'period'                : 'May 2018 - Present',
         }];
     
 });
